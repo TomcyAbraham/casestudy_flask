662b92cc2f825e573198a9dd2220ea718265f21d
TomcyAbraham <153733913+TomcyAbraham@users.noreply.github.com>
2024-04-26 17:04:42 +0000

    1

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+
+app=Flask(__name__)
+@app.route('/')
+def hello():
+    return 'hello'
+
+if __name__=='__main__':
+    app.run()
